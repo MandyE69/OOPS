60bec49448ae29a775467a652bd5e8c32c2cad2c
MandyE69 <143182397+MandyE69@users.noreply.github.com>
2023-09-10 07:30:53 +0545

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+// Write a program in C++ to input two numbers and print the sum of two numbers.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	
+	cout<<"enter a first number:";
+	cin>>a;
+	cout<<"enter a second number:";
+	cin>>b;
+	
+	cout<<"sum of two number = "<< a+b;
+	return 0;
+	
+}
